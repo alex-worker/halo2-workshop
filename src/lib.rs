ae51720bb3e13e01b5244a9307c227e79ed526cf
Alex Worker <alex@amberskynet.org>
2024-07-05 00:22:58 +0300

    try add some chip, test ok

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,15 @@
 use std::marker::PhantomData;
-
 use halo2_proofs::{
-    circuit::{Layouter, SimpleFloorPlanner, Value},
+    circuit::{Region, Layouter, SimpleFloorPlanner, Value},
     pasta::group::ff::PrimeField,
     plonk::{self, Advice, Circuit, Column, ConstraintSystem, Expression, Selector, TableColumn},
     poly::Rotation,
 };
-
-// Sets the circuit, and also stores the private input
-pub struct BracketCircuit<const L: usize, F: PrimeField> {
-    input: [char; L],
-    _p: PhantomData<F>,
-}
-
-impl<const L: usize, F: PrimeField> BracketCircuit<L, F> {
-    pub fn new(input: [char; L]) -> Self {
-        Self {
-            input,
-            _p: PhantomData,
-        }
-    }
-}
+use halo2_proofs::circuit::AssignedCell;
 
 // Stores the configuration of the table (columns) that the circuit needs
 #[derive(Clone)]
-pub struct Config {
+pub struct BracketConfig {
     s_input: Selector,
     s_not_minus_one: Selector,
     s_is_accum_zero: Selector,
@@ -36,18 +21,22 @@ pub struct Config {
     inverted_accum_plus_1: Column<Advice>,
 }
 
-impl<const L: usize, F: PrimeField> Circuit<F> for BracketCircuit<L, F> {
-    type Config = Config;
+struct BracketChip<F: PrimeField> {
+    config: BracketConfig,
+    _p: PhantomData<F>,
+}
 
-    // Not important at this stage
-    type FloorPlanner = SimpleFloorPlanner;
-
-    fn without_witnesses(&self) -> Self {
-        todo!("Not needed at this stage.")
+impl<F: PrimeField> BracketChip<F> {
+    pub fn construct(config: BracketConfig) -> Self {
+        Self {
+            config,
+            _p: PhantomData,
+        }
     }
 
-    fn configure(meta: &mut ConstraintSystem<F>) -> Self::Config {
-        let config = Config {
+    pub fn configure(meta: &mut ConstraintSystem<F>) -> BracketConfig {
+
+        let config = BracketConfig {
             s_input: meta.selector(),
             s_is_accum_zero: meta.selector(),
             s_not_minus_one: meta.selector(),
@@ -78,6 +67,7 @@ impl<const L: usize, F: PrimeField> Circuit<F> for BracketCircuit<L, F> {
             let _1 = Expression::Constant(F::ONE);
 
             let s = meta.query_selector(config.s_not_minus_one);
+
             let accum = meta.query_advice(config.accum, Rotation::cur());
             let inv_x = meta.query_advice(config.inverted_accum_plus_1, Rotation::cur());
 
@@ -98,31 +88,88 @@ impl<const L: usize, F: PrimeField> Circuit<F> for BracketCircuit<L, F> {
         config
     }
 
-    fn synthesize(
-        &self,
-        config: Self::Config,
-        mut layouter: impl Layouter<F>,
-    ) -> Result<(), plonk::Error> {
+    pub fn assign_table_allowed(&self, mut layouter: impl Layouter<F>,) {
         layouter.assign_table(
             || "allowed",
             |mut table| {
-                table.assign_cell(|| "empty", config.allowed, 0, || Value::known(F::ZERO))?;
+                table.assign_cell(|| "empty", self.config.allowed, 0, || Value::known(F::ZERO))?;
                 table.assign_cell(
                     || "(",
-                    config.allowed,
+                    self.config.allowed,
                     1,
                     || Value::known(F::from('(' as u64)),
                 )?;
                 table.assign_cell(
                     || ")",
-                    config.allowed,
+                    self.config.allowed,
                     2,
                     || Value::known(F::from(')' as u64)),
                 )?;
 
                 Ok(())
             },
+        ).unwrap();
+    }
+
+    pub fn assign_accum(&self, region: &mut Region<F>, offset: usize, value: Value<F>) -> Result<AssignedCell<F, F>, plonk::Error>{
+        let accum = region.assign_advice(|| "accum", self.config.accum, offset, || value)?;
+        Ok(accum)
+    }
+
+    pub fn assign_input(&self, region: &mut Region<F>, offset: usize, value: Value<F>) -> Result<AssignedCell<F, F>, plonk::Error>{
+        let assign_input = region.assign_advice(|| "input", self.config.input, offset, || value)?;
+        Ok(assign_input)
+    }
+
+    pub fn assign_inverted_accum_plus_1(&self, region: &mut Region<F>, accum: Value<F>, offset: usize) -> Result<AssignedCell<F, F>, plonk::Error> {
+        let value = accum.map(|val| val.add(F::ONE).invert().unwrap_or_else(|| F::ZERO));
+        let inv = region.assign_advice(
+            || "inverted accum",
+            self.config.inverted_accum_plus_1,
+            offset,
+            || value
         )?;
+        Ok(inv)
+    }
+}
+
+// Sets the circuit, and also stores the private input
+pub struct BracketCircuit<const L: usize, F: PrimeField> {
+    char_input: [char; L],
+    _p: PhantomData<F>,
+}
+
+impl<const L: usize, F: PrimeField> BracketCircuit<L, F> {
+    pub fn new(input: [char; L]) -> Self {
+        Self {
+            char_input: input,
+            _p: PhantomData,
+        }
+    }
+}
+
+impl<const L: usize, F: PrimeField> Circuit<F> for BracketCircuit<L, F> {
+    type Config = BracketConfig;
+
+    // Not important at this stage
+    type FloorPlanner = SimpleFloorPlanner;
+
+    fn without_witnesses(&self) -> Self {
+        todo!("Not needed at this stage.")
+    }
+
+    fn configure(meta: &mut ConstraintSystem<F>) -> Self::Config {
+        BracketChip::configure(meta)
+    }
+
+    fn synthesize(
+        &self,
+        config: Self::Config,
+        mut layouter: impl Layouter<F>,
+    ) -> Result<(), plonk::Error> {
+        let chip = BracketChip::construct(config.clone());
+
+        chip.assign_table_allowed(layouter.namespace(|| "assign_table_allowed row"));
 
         layouter.assign_region(
             || "",
@@ -130,41 +177,29 @@ impl<const L: usize, F: PrimeField> Circuit<F> for BracketCircuit<L, F> {
                 let _81 = Value::known(F::from(81));
                 let _2 = Value::known(F::from(2));
 
-                let prev =
-                    region.assign_advice(|| "accum", config.accum, 0, || Value::known(F::ZERO))?;
+                let prev = chip.assign_accum(&mut region, 0, Value::known(F::ZERO))?;
+                chip.assign_inverted_accum_plus_1(&mut region, prev.value().copied(), 0)?;
 
-                region.assign_advice(
-                    || "inverted accum",
-                    config.inverted_accum_plus_1,
-                    0,
-                    || prev.value().map(|val| val.add(F::ONE).invert().unwrap()),
-                )?;
                 config.s_not_minus_one.enable(&mut region, 0)?;
+                // config.s_is_accum_zero.enable(&mut region, L)?;
 
-                self.input
+                self.char_input
                     .iter()
                     .map(|sym| Value::known(F::from(*sym as u64)))
                     .enumerate()
                     .try_fold(prev.value().copied(), |prev, (offset, sym)| {
                         config.s_input.enable(&mut region, offset)?;
+                        config.s_not_minus_one.enable(&mut region, offset + 1)?;
 
-                        region.assign_advice(|| "input", config.input, offset, || sym)?;
+                        chip.assign_input(&mut region, offset, sym)?;
 
                         let acc_value = _81 - (_2 * sym) + prev;
 
-                        config.s_not_minus_one.enable(&mut region, offset + 1)?;
-                        region.assign_advice(|| "accum", config.accum, offset + 1, || acc_value)?;
-                        region.assign_advice(
-                            || "inv_accum",
-                            config.inverted_accum_plus_1,
-                            offset + 1,
-                            || acc_value.map(|v| v.add(F::ONE).invert().unwrap_or_else(|| F::ZERO)),
-                        )?;
+                        chip.assign_accum(&mut region, offset + 1, acc_value)?;
+                        chip.assign_inverted_accum_plus_1(&mut region, acc_value, offset+1)?;
 
                         Result::<_, plonk::Error>::Ok(acc_value)
                     })?;
-
-                //config.s_is_accum_zero.enable(&mut region, L)?;
 
                 Ok(())
             },
